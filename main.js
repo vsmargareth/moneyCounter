f0843126f355e5067ed2a2bd66b193e13b1da320
Vanessa_Margareth <vsmargareth@hotmail.com>
2023-04-24 01:46:03 +0100

    multiplication and print OK

---
+++ main.js
@@ -0,0 +1,74 @@
+let wrapperRef = document.querySelectorAll('.wrapper')
+let oneCentInputRef = document.querySelector('#oneCent')
+let oneCentTotalRef = document.querySelector('#oneCentTotal')
+let twoCentsInputRef = document.querySelector('#twoCents')
+let twoCentsTotalRef = document.querySelector('#twoCentsTotal')
+let fiveCentsInputRef = document.querySelector('#fiveCents')
+let fiveCentsTotalRef = document.querySelector('#fiveCentsTotal')
+let twentyCentsInputRef = document.querySelector('#twentyCents')
+let twentyCentsTotalRef = document.querySelector('#twentyCentsTotal')
+let fiftyCentsInputRef = document.querySelector('#fiftyCents')
+let fiftyCentsTotalRef = document.querySelector('#fiftyCentsTotal')
+let oneEuroInputRef = document.querySelector('#oneEuro')
+let oneEuroTotalRef = document.querySelector('#oneEuroTotal')
+let twoEurosInputRef = document.querySelector('#twoEuros')
+let twoEurosTotalRef = document.querySelector('#twoEurosTotal')
+let fiveEurosInputRef = document.querySelector('#fiveEuros')
+let fiveEurosTotalRef = document.querySelector('#fiveEurosTotal')
+let tenEurosInputRef = document.querySelector('#tenEuros')
+let tenEurosTotalRef = document.querySelector('#tenEurosTotal')
+let fiftyEurosInputRef = document.querySelector('#fiftyEuros')
+let fiftyEurosTotalRef = document.querySelector('#tenEufiftyEuros')
+let hundredEurosInputRef = document.querySelector('#hundredEuros')
+let hundredEurosTotalRef = document.querySelector('#hundredEurosTotal')
+
+
+
+// console.log(wrapperRef[0])
+
+
+// for (let item of wrapperRef) {
+//   console.log(item.childNodes[8])
+// }
+
+
+//importando o data-money
+//importante lembrar que wrapperRef não é um array ele é uma NODELIST o map não vai funcionar
+
+// const wrapperMap = wrapperRef.forEach(function (capture) {
+//   console.log(capture.dataset.money)
+// })
+
+
+function printMutiplication(number, wherePrint) {
+
+  wherePrint.innerHTML = `=  ${number}`
+
+
+}
+
+// 3- multiplica
+function multiplication(money, howMany, where) {
+  let multiplication = money * howMany
+  printMutiplication(multiplication, where)
+
+}
+
+//2- ler os inputs
+function readInput() {
+  wrapperRef.forEach(function (div) {
+    let money = div.dataset.money
+    let howMany = div.childNodes[3].value
+    let where = div.childNodes[5]
+    console.log(where)
+
+    multiplication(money, howMany, where)
+  })
+}
+
+//começa aqui: 'escutando' as teclas
+wrapperRef.forEach(function (array) {
+  array.addEventListener('keyup', readInput)
+})
+
+//mapeando o wrapper map
